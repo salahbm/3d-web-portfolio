b1987e450f0c01f43c06820d25b54bb299d61df8
salahbm <salahbm.001@gmail.com>
2023-11-24 10:25:32 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from './components/Navbar';
+import './index.css'
+import {  Route, BrowserRouter as Router, Routes} from "react-router-dom";
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
+import Contacts from './pages/Contacts';
+const App = () => {
+  return (
+<main className='bg-slate-300/20'>
+  <Router>
+<Navbar/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/projects' element={<Projects/>}/>
+      <Route path='/contact' element={<Contacts/>}/>
+    </Routes>
+  </Router>
+
+</main>
+  )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React, { Suspense } from 'react'
+import { Canvas } from "@react-three/fiber";
+const Home = () => {
+  return (
+<section className='w-full h-screen relative'>
+    {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>    pop up</div> */}
+<Canvas className='w-full h-screen bg-transparent'
+camera={{near:0.1, far:1000}}> 
+<Suspense fallback={<Loader/>}>
+
+</Suspense>
+
+</Canvas>
+
+</section>
+  )
+}
+
+export default Home
