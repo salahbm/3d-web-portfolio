3934ed3cb6d48dbf528d83ec074c635a1795e073
salahbm <salahbm.001@gmail.com>
2023-11-24 10:53:42 +0900

    add Island in Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,20 @@ import { Canvas } from "@react-three/fiber";
 import Loader from '../components/Loader';
 import IslandModel from '../models/Island';
 const Home = () => {
+const adjustIslandForScreenSize =   ()=>{
+let screenScale=null;
+ let screenPosition=[0,-6.5, -43]
+ let rotation = [1, 0.47,0 ]
+if (window.innerWidth <768 ) {
+    screenScale=[ 0.9,0.9,0.9]
+
+    
+}else{
+    screenScale=[ 1, 1,1]
+}
+return [screenPosition, screenScale,rotation]
+}  
+const [isIslandPosition, isIslandScale,isIslandRotation]=adjustIslandForScreenSize()
   return (
 <section className='w-full h-screen relative'>
     {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>    pop up</div> */}
@@ -16,7 +30,7 @@ camera={{near:0.1, far:1000}}>
 <pointLight/>
 <spotLight/>
 <hemisphereLight/>
-<IslandModel/>
+<IslandModel position={isIslandPosition} scale={isIslandScale} rotation={isIslandRotation}/>
 </Canvas>
 
 </section>
